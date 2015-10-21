8dabaf6d7e267bc1cbe6a262dbdfd5c495e90e72
raditotev <raditotev@gmail.com>
2015-10-21 12:35:07 +0100

    Add description to exercises

--- multiples_3_5.rb
+++ multiples_3_5.rb
@@ -1,9 +1,15 @@
-natural_numbers = []
+# If we list all the natural numbers below 10 that are multiples
+# of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+# Find the sum of all the multiples of 3 or 5 below 1000.
 
-for x in 1..999
-	if x % 3 == 0 or x % 5 == 0
-		natural_numbers << x
+def natural_numbers
+	natural_numbers = []
+	for x in 1..999
+		if x % 3 == 0 or x % 5 == 0
+			natural_numbers << x
+		end
 	end
 end
 
 puts natural_numbers.inject(0) {|sum, x| sum + x}
+# => 499500
