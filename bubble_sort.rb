407b1ef334553e78241d310ada75b8b0e1e1f0cf
raditotev <raditotev@gmail.com>
2015-10-23 13:56:06 +0100

    Bubble sort method

---
+++ bubble_sort.rb
@@ -0,0 +1,34 @@
+# def bubble_sort arr
+# 	n = arr.length
+# 	count  = 0
+# 	begin
+# 		swapped = false
+# 		for i in 1..n-1
+# 			if arr[i - 1] > arr[i]
+# 				arr[i - 1], arr[i] = arr[i], arr[i - 1]
+# 				swapped = true
+# 			end
+# 		end
+# 	end while swapped
+# 	arr
+# end
+
+# Optimized bubble_sort
+def bubble_sort arr
+	n = arr.size
+	swapped = true
+	while swapped
+		swapped = false
+		for i in 1..n-1
+			if arr[i-1] >arr[i]
+				arr[i-1], arr[i] = arr[i], arr[i-1]
+				swapped = true
+			end
+		end
+		n -= 1
+	end
+	arr
+end
+
+
+p bubble_sort([3,4,5,2,1])
